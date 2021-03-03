1c74dc6dff74fbb550230c9d271c37e27db836d9
hyeji choi <hjchoi2323@gmail.com>
2021-03-03 23:49:01 +0900

    first commit

---
+++ test2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello python")
+
+
+# In[2]:
+
+
+a=5
+b=10
+
+
+# In[3]:
+
+
+a+b
+
+
+# In[4]:
+
+
+a=5
+b=10
+
+
+# In[5]:
+
+
+a+b
+
+
+# In[ ]:
+
+
+
+
